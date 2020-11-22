a16ab54034192af7d3b611b377a3dcfd65b0f140
TheAnosmic <theanosmic@gmail.com>
2020-11-22 21:40:36 +0200

    Added SampleAnimationFrame editor utility

--- EditorUtils/SampleAnimationFrame.cs
+++ EditorUtils/SampleAnimationFrame.cs
@@ -1,7 +1,31 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Utils.EditorUtils
 {
-    public class SampleAnimationFrame
+    [ExecuteInEditMode]
+    public class SampleAnimationFrame : MonoBehaviour
     {
+        #if UNITY_EDITOR
+
+        [Range(0, 60)]
+        [SerializeField] private int _frame;
+        [SerializeField] private List<GameObject> _objects;
         
+
+        private void OnValidate()
+        {
+            //Debug.Log(_frame);
+            foreach (var o in _objects)
+            {
+                var clipInfo = o.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0];
+                var clip = clipInfo.clip;
+                var time = _frame / clip.frameRate;
+                clip.SampleAnimation(o, time);
+            }
+        }
+        
+        #endif
     }
 }
