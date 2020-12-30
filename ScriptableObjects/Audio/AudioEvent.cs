e6c5aadf79a18bca687c38ed26bcfc2a2d3badd3
TheAnosmic <theanosmic@gmail.com>
2020-12-30 15:32:54 +0200

    added scriptable objects audio event

---
+++ ScriptableObjects/Audio/AudioEvent.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Utils.ScriptableObjects.Audio
+{
+	public abstract class AudioEvent : ScriptableObject
+	{
+		public abstract void Play(AudioSource source);
+	}
+}
---
+++ ScriptableObjects/Audio/RangedFloat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Utils.ScriptableObjects.Audio
+{
+	[Serializable]
+	public struct RangedFloat
+	{
+		public float minValue;
+		public float maxValue;
+	}
+}
